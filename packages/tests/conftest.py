6b86d1ea05d0cd384adb542998902a9d66540a4e
claire muwanguzi <kclaire56@yahoo.com>
2022-09-02 19:04:57 -0700

    add preprosessing tests

--- packages/tests/conftest.py
+++ packages/tests/conftest.py
@@ -17,5 +17,31 @@ def sample_input_data():
     data = data.head(20)
     return data
 
-# @pytest.mark.parameterize(
-#     'size' , [4,5,7])
+
+@pytest.fixture(scope="session")
+def pipeline_inputs():
+    # For larger datasets, here we would use a testing sub-sample.
+    data = load_dataset(file_name=config.app_config.training_data_file)
+
+    # Divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.model_config.features],  # predictors
+        data[config.model_config.target],
+        test_size=config.model_config.test_size,
+        # we are setting the random seed here
+        # for reproducibility
+        random_state=config.model_config.random_state,
+    )
+
+    return X_train, X_test, y_train, y_test
+
+
+@pytest.fixture()
+def raw_training_data():
+    # For larger datasets, here we would use a testing sub-sample.
+    return load_dataset(file_name=config.app_config.training_data_file)
+
+
+@pytest.fixture()
+def sample_input_data():
+    return load_dataset(file_name=config.app_config.test_data_file)
--- packages/tests/test_inputdata.py
+++ packages/tests/test_inputdata.py
@@ -1,34 +1,34 @@
-# import sys , os
-import pytest
-from packages.src.utils import *
-import pandas as pd 
-from sklearn.model_selection import train_test_split
+# # import sys , os
+# import pytest
+# from packages.src.utils import *
+# import pandas as pd 
+# from sklearn.model_selection import train_test_split
 
-def test_status_unique_values(sample_input_data):
-    # given 
-    data = sample_input_data
-    unique_values = data['status'].unique()
-    assert len(unique_values) == 2
+# def test_status_unique_values(sample_input_data):
+#     # given 
+#     data = sample_input_data
+#     unique_values = data['status'].unique()
+#     assert len(unique_values) == 2
 
 
-@pytest.mark.parametrize(
-    'size' , [10,5,7])
-def test_status_unique_values_multiple(sample_input_data, size):
-    # given 
-    data = sample_input_data.head(size)
-    unique_values = data['status'].unique()
-    assert len(unique_values) == 2
+# @pytest.mark.parametrize(
+#     'size' , [10,5,7])
+# def test_status_unique_values_multiple(sample_input_data, size):
+#     # given 
+#     data = sample_input_data.head(size)
+#     unique_values = data['status'].unique()
+#     assert len(unique_values) == 2
 
-# class TestTraindata:
-#     @pytest.fixture
-#     def get_train(sample_input_data):
-#         X_train, X_test, y_train, y_test = train_test_split(X,y, test_size =.3, random_state = 0)
-#         train = X_train[:30]
-#     def test_train(train):
-#         assert train.shape[0] == 30
-#     def test_train2(train):
-#         print(type(train))
-#         assert type(train) == 'class'
+# # class TestTraindata:
+# #     @pytest.fixture
+# #     def get_train(sample_input_data):
+# #         X_train, X_test, y_train, y_test = train_test_split(X,y, test_size =.3, random_state = 0)
+# #         train = X_train[:30]
+# #     def test_train(train):
+# #         assert train.shape[0] == 30
+# #     def test_train2(train):
+# #         print(type(train))
+# #         assert type(train) == 'class'
         
         
 
