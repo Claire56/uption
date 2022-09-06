113b01f6a8afbfc66c0ca4800871fa3c17ca8fb9
claire muwanguzi <kclaire56@yahoo.com>
2022-09-05 17:18:10 -0700

    added config tests

--- packages/setup.py
+++ packages/setup.py
@@ -1,63 +1,63 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
 
-from pathlib import Path
+# from pathlib import Path
 
-from setuptools import find_packages, setup
+# from setuptools import find_packages, setup
 
-# Package meta-data.
-NAME = 'startup-classifier'
-DESCRIPTION = "Classifier model for startups from Train In Data."
-URL = "https://github.com/claire56/uption"
-EMAIL = "kclamu26@gmail.com"
-AUTHOR = "Zimba- Claire MK"
-REQUIRES_PYTHON = ">=3.6.0"
-
-
-# What packages are required for this module to be executed?
-def list_reqs(fname="requirements.txt"):
-    with open(fname) as fd:
-        return fd.read().splitlines()
+# # Package meta-data.
+# NAME = 'startup-classifier'
+# DESCRIPTION = "Classifier model for startups from Train In Data."
+# URL = "https://github.com/claire56/uption"
+# EMAIL = "kclamu26@gmail.com"
+# AUTHOR = "Zimba- Claire MK"
+# REQUIRES_PYTHON = ">=3.6.0"
 
 
-long_description = DESCRIPTION
-
-# Load the package's VERSION file as a dictionary.
-about = {}
-ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / 'src'
-with open(PACKAGE_DIR / "VERSION") as f:
-    _version = f.read().strip()
-    about["__version__"] = _version
+# # What packages are required for this module to be executed?
+# def list_reqs(fname="requirements.txt"):
+#     with open(fname) as fd:
+#         return fd.read().splitlines()
 
 
-# Where the magic happens:
-setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=("tests",)),
-    package_data={"gb_classifier": ["VERSION"]},
-    install_requires=list_reqs(),
-    extras_require={},
-    include_package_data=True,
-    license="BSD-3",
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-)
+# long_description = DESCRIPTION
+
+# # Load the package's VERSION file as a dictionary.
+# about = {}
+# ROOT_DIR = Path(__file__).resolve().parent
+# PACKAGE_DIR = ROOT_DIR / 'src'
+# with open(PACKAGE_DIR / "VERSION") as f:
+#     _version = f.read().strip()
+#     about["__version__"] = _version
+
+
+# # Where the magic happens:
+# setup(
+#     name=NAME,
+#     version=about["__version__"],
+#     description=DESCRIPTION,
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     author=AUTHOR,
+#     author_email=EMAIL,
+#     python_requires=REQUIRES_PYTHON,
+#     url=URL,
+#     packages=find_packages(exclude=("tests",)),
+#     package_data={"gb_classifier": ["VERSION"]},
+#     install_requires=list_reqs(),
+#     extras_require={},
+#     include_package_data=True,
+#     license="BSD-3",
+#     classifiers=[
+#         # Trove classifiers
+#         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+#         "License :: OSI Approved :: MIT License",
+#         "Programming Language :: Python",
+#         "Programming Language :: Python :: 3",
+#         "Programming Language :: Python :: 3.6",
+#         "Programming Language :: Python :: 3.7",
+#         "Programming Language :: Python :: 3.8",
+#         "Programming Language :: Python :: Implementation :: CPython",
+#         "Programming Language :: Python :: Implementation :: PyPy",
+#     ],
+# )
--- packages/tests/test_config.py
+++ packages/tests/test_config.py
@@ -1,129 +1,134 @@
-# import pytest
-# from packages.src.utils import DataDetails , Utilities
-# import pandas as pd 
+import pytest
+from packages.src.utils import DataDetails , Utilities
+import pandas as pd 
 
 
-# from pathlib import Path
+from pathlib import Path
 
-# from gradient_boosting_model.config.core import (
-#     create_and_validate_config,
-#     fetch_config_from_yaml,
-# )
+from packages.src.config.core import (
+    create_and_validate_config,
+    fetch_config_from_yaml,
+)
 
-# import pytest
-# from pydantic import ValidationError
+import pytest
+from pydantic import ValidationError
 
 
-# TEST_CONFIG_TEXT = """
-# package_name: gradient_boosting_model
-# training_data_file: houseprice.csv
-# test_data_file: test.csv
-# drop_features: YrSold
-# pipeline_name: gb_regression
-# pipeline_save_file: gb_regression_output_v
-# target: SalePrice
-# variables_to_rename:
-#     foo: bar
-# test_size: 0.1
-# features:
-#   - LotArea
-# numerical_vars:
-#   - LotArea
-# categorical_vars:
-#   - BsmtQual
+TEST_CONFIG_TEXT = """
+package_name: src
+training_data_file: startup_data.csv
+test_data_file: test.csv
+drop_features:
+    - name
+pipeline_name: startup_classifier
+pipeline_save_file: startup_classifier_output_v
+target: labels
+variables_to_rename:
+    has_roundA: has_roundA
+test_size: 0.1
+features:
+  - relationships
+numerical_vars:
+  - relationships
+categorical_vars:
+  - category_code
+one_hot_encoding_vars:
+  - category_code
+numerical_vars_with_na:
+  - age_first_milestone_year
+numerical_na_not_allowed:
+  - is_CA
+random_state: 0
+n_estimators: 50
+rare_label_tol: 0.01
+rare_label_n_categories: 5
+loss: log_loss
+allowed_loss_functions:
+  - log_loss
+  
+"""
+
+INVALID_TEST_CONFIG_TEXT = """
+package_name: src
+training_data_file: startup_data.csv
+test_data_file: test.csv
+drop_features: 
+    - name
+pipeline_name: startup_classifier
+pipeline_save_file: startup_classifier_output_v
+target: labels
+features:
+  - relationships
+numerical_vars:
+  - relationships
+categorical_vars:
+  - category_codeal
 # temporal_vars: YearRemodAdd
-# numerical_vars_with_na:
-#   - LotFrontage
-# numerical_na_not_allowed:
-#   - LotArea
-# random_state: 0
-# n_estimators: 50
-# rare_label_tol: 0.01
-# rare_label_n_categories: 5
-# loss: ls
-# allowed_loss_functions:
-#   - log_loss
-#   
-# """
-
-# INVALID_TEST_CONFIG_TEXT = """
-# package_name: gradient_boosting_model
-# training_data_file: houseprice.csv
-# test_data_file: test.csv
-# drop_features: YrSold
-# pipeline_name: gb_regression
-# pipeline_save_file: gb_regression_output_v
-# target: SalePrice
-# features:
-#   - LotArea
-# numerical_vars:
-#   - LotArea
-# categorical_vars:
-#   - BsmtQual
-# temporal_vars: YearRemodAdd
-# numerical_vars_with_na:
-#   - LotFrontage
-# numerical_na_not_allowed:
-#   - LotArea
-# random_state: 0
-# n_estimators: 50
-# rare_label_tol: 0.01
-# rare_label_n_categories: 5
-# loss: ls
-# allowed_loss_functions:
-#   - huber
-# """
+numerical_vars_with_na:
+  - age_first_milestone_year
+numerical_na_not_allowed:
+  - is_CA
+one_hot_encoding_vars:
+  - category_code
+random_state: 0
+n_estimators: 50
+rare_label_tol: 0.01
+rare_label_n_categories: 5
+loss: ls
+allowed_loss_functions:
+  - huber
+"""
 
 
-# def test_fetch_config_structure(tmpdir):
-#     # Given
-#     # We make use of the pytest built-in tmpdir fixture
-#     configs_dir = Path(tmpdir)
-#     config_1 = configs_dir / "sample_config.yml"
-#     config_1.write_text(TEST_CONFIG_TEXT)
-#     parsed_config = fetch_config_from_yaml(cfg_path=config_1)
+def test_fetch_config_structure(tmpdir):
+    # Given
+    # We make use of the pytest built-in tmpdir fixture
+    configs_dir = Path(tmpdir)
+    config_1 = configs_dir / "sample_config.yml"
+    config_1.write_text(TEST_CONFIG_TEXT)
+    parsed_config = fetch_config_from_yaml(cfg_path=config_1)
 
-#     # When
-#     config = create_and_validate_config(parsed_config=parsed_config)
+    # When
+    config = create_and_validate_config(parsed_config=parsed_config)
 
-#     # Then
-#     assert config.model_config
-#     assert config.app_config
-
-
-# def test_config_validation_raises_error_for_invalid_config(tmpdir):
-#     # Given
-#     # We make use of the pytest built-in tmpdir fixture
-#     configs_dir = Path(tmpdir)
-#     config_1 = configs_dir / "sample_config.yml"
-
-#     # invalid config attempts to set a prohibited loss
-#     # function which we validate against an allowed set of
-#     # loss function parameters.
-#     config_1.write_text(INVALID_TEST_CONFIG_TEXT)
-#     parsed_config = fetch_config_from_yaml(cfg_path=config_1)
-
-#     # When
-#     with pytest.raises(ValidationError) as excinfo:
-#         create_and_validate_config(parsed_config=parsed_config)
-
-#     # Then
-#     assert "not in the allowed set" in str(excinfo.value)
+    # Then
+    assert config.model_config
+    assert config.app_config
 
 
-# def test_missing_config_field_raises_validation_error(tmpdir):
-#     # Given
-#     # We make use of the pytest built-in tmpdir fixture
-#     configs_dir = Path(tmpdir)
-#     config_1 = configs_dir / "sample_config.yml"
-#     TEST_CONFIG_TEXT = """package_name: gradient_boosting_model"""
-#     config_1.write_text(TEST_CONFIG_TEXT)
-#     parsed_config = fetch_config_from_yaml(cfg_path=config_1)
+def test_config_validation_raises_error_for_invalid_config(tmpdir):
+    # Given
+    # We make use of the pytest built-in tmpdir fixture
+    configs_dir = Path(tmpdir)
+    config_1 = configs_dir / "sample_config.yml"
 
-#     # When
-#     with pytest.raises(ValidationError) as excinfo:
-#         create_and_validate_config(parsed_config=parsed_config)
+    # invalid config attempts to set a prohibited loss
+    # function which we validate against an allowed set of
+    # loss function parameters.
+    config_1.write_text(INVALID_TEST_CONFIG_TEXT)
+    parsed_config = fetch_config_from_yaml(cfg_path=config_1)
 
-#     # Then
-#     assert "field required" in str(excinfo.value)
-#     assert "pipeline_name" in str(excinfo.value)
+    # When
+    with pytest.raises(ValidationError) as excinfo:
+        create_and_validate_config(parsed_config=parsed_config)
+
+    # Then
+    assert "not in the allowed set" in str(excinfo.value)
+
+
+def test_missing_config_field_raises_validation_error(tmpdir):
+    # Given
+    # We make use of the pytest built-in tmpdir fixture
+    configs_dir = Path(tmpdir)
+    config_1 = configs_dir / "sample_config.yml"
+    TEST_CONFIG_TEXT = """package_name: src"""
+    config_1.write_text(TEST_CONFIG_TEXT)
+    parsed_config = fetch_config_from_yaml(cfg_path=config_1)
+
+    # When
+    with pytest.raises(ValidationError) as excinfo:
+        create_and_validate_config(parsed_config=parsed_config)
+
+    # Then
+    assert "field required" in str(excinfo.value)
+    assert "pipeline_name" in str(excinfo.value)
